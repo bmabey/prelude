c015d0cd6ceda17754eeadb13b745caf74b8f622
Ben Mabey <ben@benmabey.com>
2014-09-27 16:16:40 -0600

    adds flatland theme and window-numbering

---
+++ personal/init.el
@@ -0,0 +1,24 @@
+(prelude-require-packages '(window-numbering flatland-theme))
+
+(require 'window-numbering)
+(window-numbering-mode 1)
+
+(global-set-key (kbd "s-0") 'select-window-0)
+(global-set-key (kbd "s-1") 'select-window-1)
+(global-set-key (kbd "s-2") 'select-window-2)
+(global-set-key (kbd "s-3") 'select-window-3)
+(global-set-key (kbd "s-4") 'select-window-4)
+(global-set-key (kbd "s-5") 'select-window-5)
+(global-set-key (kbd "s-6") 'select-window-6)
+(global-set-key (kbd "s-7") 'select-window-7)
+(global-set-key (kbd "s-8") 'select-window-8)
+(global-set-key (kbd "s-9") 'select-window-9)
+
+
+(load-theme 'flatland)
+
+
+(global-set-key (kbd "s-;") 'comment-dwim)
+
+
+(server-start)
